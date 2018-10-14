0c4fde3e057b38448808efef39cd22125a952eaf
Ilie-Ablachim Denis <denis.ilie_ablachim@lummetry.ai>
2018-10-14 20:27:02 +0300

    Added timestamp

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ Created on Sat Sep 29 16:01:39 2018
 
 import json
 import urllib.request
+import datetime
 
 from pprint import pprint
 from operator import itemgetter
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import column
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import DatetimeTickFormatter
 from bokeh.palettes import Dark2_5 as palette
 
 # read input data
@@ -31,6 +33,7 @@ output_file("lines.html")
 # bokeh tools
 tools_to_show = 'hover, box_zoom, pan, save, reset, wheel_zoom'
 
+
 # printare sesiuni
 for dIndex, dValue in enumerate(data):
     
@@ -41,14 +44,21 @@ for dIndex, dValue in enumerate(data):
     tmp_data = sorted(tmp_data, key=itemgetter(0))
     
     # define figure
-    p = figure(plot_width=1200, plot_height=500, tools=tools_to_show,
+    p = figure(plot_width=1200, plot_height=500, tools=tools_to_show, x_axis_type="datetime",
                title="HRV evolution", x_axis_label='timestamp', y_axis_label='HRV')
     
     # extract RTC and HRV
-    x = [i[0] for i in tmp_data] # RTC
+    x = [datetime.datetime.fromtimestamp(int(i[0])) for i in tmp_data] # RTC
     #x = [time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(tmp_x))) for tmp_x in x]
-    y = [i[1] for i in tmp_data] # HRV
+    y = [int(i[1]) for i in tmp_data] # HRV
     
+    p.xaxis.formatter=DatetimeTickFormatter(
+            days=["%H:%M:%S %d %B %Y"],
+            months=["%H:%M:%S %d %B %Y"],
+            hours=["%H:%M:%S %d %B %Y"],
+            minutes=["%H:%M:%S %d %B %Y"]
+        )
+
     source = ColumnDataSource(data={
                 'x': x, 
                 'y': y
